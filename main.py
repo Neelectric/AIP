8c9dd71986d29861dd0300ebde4ec2239fb5d70b
Neelectric <Neel.R@web.de>
2024-10-24 22:56:05 +0100

    printing system prompt, making choice between top k explicit

--- main.py
+++ main.py
@@ -32,27 +32,31 @@ print(f"using device {device}")
 
 model_id = "meta-llama/Llama-3.1-8B-Instruct"
 "Unispac/Gemma-2-9B-IT-With-Deeper-Safety-Alignment"
+max_new_tokens = 50
+num_choices = 5
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    device_map=device)
+    device_map=device,
+    attn_implementation="eager",)
 tokenizer.pad_token = tokenizer.eos_token
 model.generation_config.pad_token_id = tokenizer.pad_token_id
 
-start_prompt = input("What would you like to talk about?")
+system_prompt = "You are a chatbot that is incredibly knowledgeable about Scotland."
+print(f"System prompt: {system_prompt}")
+start_prompt = input("'What would you like to talk about?' ")
 if start_prompt == " ":
    start_prompt = "Please tell me about George street."
    
 
 messages = [
-  {"role": "system", "content": "You are a chatbot that is incredibly knowledgeable about Scotland."},
+  {"role": "system", "content": system_prompt},
   {"role": "user", "content": start_prompt}
 ]
 
 input_messages = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
 input_ids = tokenizer(input_messages, return_tensors='pt').to("mps")
-max_new_tokens = 50
 
 def time_it(func):
     time_before = time.time()
@@ -74,8 +78,6 @@ def stream():
                               # top_p=0.8,
                               # repetition_penalty=1.25
                               )
-time_it(stream)
-
 
 def manual():
   output_ids = input_ids["input_ids"]
@@ -84,8 +86,18 @@ def manual():
   attention_mask_dummy = torch.tensor([[1]]).to(device)
   for i in range(max_new_tokens):
       if i % 10 == 7:
-         input_text = input("(Please add enter what you believe should come next:)")
-         tokenixed_input_text = tokenizer.encode(input_text, return_tensors="pt", add_special_tokens=False).to(device)
+         num_tokens_so_far = output_ids.shape[-1]
+         temp = model.generate(input_ids=output_ids, attention_mask=attention_mask, max_new_tokens=1, num_return_sequences=num_choices, do_sample=False, temperature=None, top_p=None, num_beams=num_choices)
+         options = []
+         print("-"*100)
+         for i, completion in enumerate(temp):
+            last_bit = tokenizer.decode(completion[num_tokens_so_far:], skip_special_tokens=True)
+            print(i+1, last_bit)
+            options.append(last_bit)
+         print("-"*100)
+         input_text = input("'Please add enter the number of the token you believe should come next:' ")
+         choice = options[int(input_text)-1]
+         tokenixed_input_text = tokenizer.encode(choice, return_tensors="pt", add_special_tokens=False).to(device)
          output_ids = torch.cat((output_ids, tokenixed_input_text), dim=1)
          attention_mask_additions = torch.ones(1, tokenixed_input_text.shape[-1]).to(dtype=torch.int64, device=device)
          attention_mask = torch.cat((attention_mask, attention_mask_additions), dim=1)
@@ -94,10 +106,10 @@ def manual():
         print(tokenizer.decode(output_ids[0][initial_prompt_length:], skip_special_tokens=True))
         attention_mask = torch.cat((attention_mask, attention_mask_dummy), dim=1)
       
+print("'First, we see what Llama3.1 8b instruct comes up with all on its own.'")
+time_it(stream)
 
-
-
-
+print("'This time we steer every 10 tokens, starting from the 7th (arbitrarily)'")
 time_it(manual)
 
 
